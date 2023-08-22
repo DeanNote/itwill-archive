251deffdeb5f1c8c62032305e6fda22c381056e8
Seokcoding <142768477+Git-Seokcoding@users.noreply.github.com>
2023-08-22 16:44:33 +0900

    Add files via upload

---
+++ (dataType)Ex2.java
@@ -0,0 +1,17 @@
+package dataType;
+
+public class Ex2 {
+
+	public static void main(String[] args) {
+		// 2진수, 8진수, 10진수, 16진수 표현 방법
+		// 1. 10진수 : 일반적인 정수 그대로 표현
+		int num = 10; // 10진수 10을 저장
+		System.out.println(num);
+		
+		// 2. 2진수 : 0 또는 1 숫자만 사용하며, 접두사 0b 붙음
+		num = 0b1010;
+		System.out.println(num);
+
+	}
+
+}
