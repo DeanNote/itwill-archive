0ddfdb369a182483a2368546b88742ceb5aa8aea
Git-Seokcoding <alstjr3824@naver.com>
2023-08-29 14:47:13 +0900

    8/29(화) Java 6교시

---
+++ JAVA0829/src/if_/Ex1.java
@@ -0,0 +1,30 @@
+package if_;
+
+public class Ex1 {
+
+	public static void main(String[] args) {
+		/*
+		 * 조건문
+		 * - 조건식 결과에 따라 실행 할 문장이 달라지는 문(statement)
+		 * - if문과 switch문으로 구분됨
+		 * 
+		 * [ if문 ]
+		 * 1. if문(단일 if문)
+		 * - 가장 단순한 조건문
+		 * - 조건식 판별 결과가 true 일 때 특정 문장(블록) 추가로 실행
+		 * 
+		 * <기본 문법>
+		 * 문장 1;
+		 * 
+		 * if(조건식 "true or false 나타낼수 있는 문장")
+		 * { 
+		 * 조건식 판별 결과가 
+		 * }
+		 * 
+		 * 
+		 * 
+		 */
+		
+	}
+
+}
