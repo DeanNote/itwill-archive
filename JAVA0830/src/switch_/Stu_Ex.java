764a56587336e46c8234ea9be40c2af33fd93ad9
Git-Seokcoding <alstjr3824@naver.com>
2023-08-30 15:05:59 +0900

    8/30(수) 다중 if문

---
+++ JAVA0830/src/switch_/Stu_Ex.java
@@ -0,0 +1,55 @@
+package switch_;
+
+public class Stu_Ex {
+
+	public static void main(String[] args) {
+		/*
+		 * switch문 (switch ~ case 문)
+		 * - if문 처럼 특정 조건에 대해 여러가지 판별을 수행하는 조건문
+		 *   => 단, if문과는 달리 범위에 대해 판별 등이 불가능하며
+		 *      값에 대한 동등 비교만 가능함
+		 * - switch 문의 조건식에는 연산식 도는 변수, 리터럴 등이 올 수 있으나
+		 *   반드시 정수 또는 문자열(또는 enum 타입 변수)만 사용 가능     
+		 *  =>  이 중 정수는 byte, short, char, int 만 사용가능
+		 * - s
+		 *  
+		 *  
+		 *  
+		 *  
+		 *  
+		 *  
+		 *  
+		 *  
+		 * <기본문법>
+		 */
+//		switch (조건식) {// 조건식에는 정수, 문자열(또는 enum 타입 상수)만 사용 
+//		case value(값1):
+//		    조건식 결과가 값1과 일치하는 경우 실행할 문장들...
+//		case value2(값2):
+//		    조건식 결과가 값2와 일치하는 경우 실행할 문장들...
+//			break;
+//
+//		default:
+//			break;
+//		}
+		
+		int num = 2;
+		
+		switch(num) {
+			case 1 : System.out.println("num = 1");
+			case 2 : System.out.println("num = 2");
+		}
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+
+}
