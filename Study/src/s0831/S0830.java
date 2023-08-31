9fe65fa62ff19090b8faea2e0c5ddfa2f7b3fee1
Git-Seokcoding <alstjr3824@naver.com>
2023-08-31 16:59:12 +0900

    8/31 자습

---
+++ Study/src/s0831/S0830.java
@@ -0,0 +1,16 @@
+package s0831;
+
+public class S0830 {
+
+	public static void main(String[] args) {
+		
+		int dan = 2;
+		int i = 1;
+		System.out.println(" < " + dan + "단 >");
+		for(i = 1; i < 10; i++) {
+			System.out.printf("%d * %d = %d\n", dan, i, (dan * i));
+		}
+	}
+}	
+
+
---
+++ Study/src/s0831/S0831.java
@@ -0,0 +1,16 @@
+package s0831;
+
+public class S0831 {
+
+	public static void main(String[] args) {
+
+	int i, j;
+	int temp;
+	ar = {75, 95, 85, 100, 50};
+	
+		
+		
+		
+		
+	}	
+}
