35c1641e4c2db2c904597dc847938c4a0e0c613d
Git-Seokcoding <alstjr3824@naver.com>
2023-09-04 11:20:41 +0900

    9/4(월) 중첩 for문

--- JAVA0904/src/for_/Stu_Test3.java
+++ JAVA0904/src/for_/Stu_Test3.java
@@ -23,12 +23,63 @@ public class Stu_Test3 {
 		
 		for(int m = 0; m <= 59; m++) {
 			for(int s = 0; s <= 59; s++) {
-				System.out.println(m+"분 " + s+"초");
+				System.out.printf("%d분%2d초\n",m,s);
 			}
-			
 		}	
+		System.out.println("---------------------------------------");
+		/*
+		 * 중첩 for 문 활용한 구구단 전체 출력(2단 ~ 9단)
+		 * <2단>
+		 * 2*1=2
+		 * 2*2=3
+		 * ~~생략
+		 * 2*9=18
+		 * <3단>
+		 * 3*1=3
+		 * ~~~생략
+		 * <9단>
+		 * ~~생략
+		 * 9*9=81
+		 */
+		int dan = 0;
+		for(dan = 2; dan<=9; dan++) {
+			System.out.println("<" + dan + "단>");
+			for(int i =1; i<=9; i++) {
+//				System.out.println(dan + "*" + i + "=" + dan*i);
+				System.out.printf("%d + %d = %d\n",dan,i,(dan*i));
+			}
+		  System.out.println();
+		}
+		System.out.println("---------------------------------------");
+
+		//주사위 2개를 굴려서 주사위 눈의 합이 6일 때 두 주사위 값을 출력
+		//ex) 주사위 1         주사위 2        합계
+		/*      1        +       5       =       6
+		/*      2        +       4       =       6
+		/*      3        +       3       =       6
+		/*      4        +       2       =       6
+		/*      5        +       1       =       6
+		 * 주사위 눈 : 1~6
+		 * 주사위(dice1, dice2) 변수 값을 1~6까지 1씩 증가하면서 반복하여
+		 * 주사위 눈의 합계가 6인지 판별하여 6일 때 주사위 눈의 값 출력
+		 * => 판별해야 하므로 if문이 같이 사용됨
+		*/
+	
+		int dice1 = 0;
+		int dice2 = 0;
+		for(dice1=1; dice1<=6; dice1++) {
+			for(dice2=1; dice2<=6; dice2++) {
+				if(dice1 + dice2 == 6) {
+					System.out.printf("%d + %d = %d\n",dice1,dice2,(dice1+dice2));
+				}
+			}
+		}
 		
-	     
- }
+		
+	
+	
+	
+	
+	}
 }
 
