e4bebbff31dfe7f27bfc2a8edd11ccd7e30695ed
Git-Seokcoding <alstjr3824@naver.com>
2023-09-04 12:24:27 +0900

    9/4(월) for문 예시 while문으로 바꿔보기

---
+++ JAVA0904/src/while_/Stu_Test.java
@@ -0,0 +1,26 @@
+package while_;
+
+public class Stu_Test {
+
+	public static void main(String[] args) {
+		/* while문을 이용하여 구구단 2단 출력
+		 * 
+		 */
+		
+		int dan = 2;
+		int i = 1;
+		while(i<10) {
+			System.out.printf("%d * %d = %d\n",dan,i,(dan*i));
+			i++;
+		}
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+
+}
