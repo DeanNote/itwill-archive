ad42366b988dff9a9efdd6573a7559c15d0ab243
Git-Seokcoding <alstjr3824@naver.com>
2023-09-05 16:08:57 +0900

    9/5(화) java break, continue

---
+++ JAVA0905/src/continue_break/Stu_Ex2.java
@@ -0,0 +1,61 @@
+package continue_break;
+
+public class Stu_Ex2 {
+
+	public static void main(String[] args) {
+		// 중첩 for문을 사용하여 구구단 2단~9단 출력
+		// => 단, 구구단의 단이 5단일 때 반복문 종료
+//		int dan = 2;
+//		while(dan<10) {
+//			if(dan==5) {
+//				break;
+//			}
+//			System.out.printf("< %d단 >\n",dan);
+//			
+//			int s = 1;
+//			while(s<10) {
+//				System.out.printf("%2d * %2d = %2d\n",dan,s,dan*s);
+//				s++;
+//			}
+//			dan++;
+//			System.out.println();
+//		}
+		
+		
+		/*
+		 * Label 기능
+		 * - 반복문의 앞에 위치하여 break 또는 continue 의 동작 지점을 지정
+		 * - break문 또는 continue 문 뒤에 해당 레이블의 이름을 지정할 경우
+		 * 	 Label 위치의 다음 반복문에 break 또는 contibue 문이 적용됨
+		 * - 주로, 중첩 반복문에서 반복문을 한꺼번에 빠져나가기 위해 사용
+		 * - 반복문 앞 또는 위에 주로 대문자로 된 문자를 작성하고 콜론을 붙임
+		 * 
+		 * <기본 문법>
+		 * 레이블명 : 
+		 * 바깥쪽 반복문(){
+		 * 	안쪽 반복문(){
+		 * 		if(조건식){
+		 * 			break; 레이블 명		
+		 * 		}
+		 * 	  }
+		 * }
+		 * 
+		 */
+		
+			
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+
+}
