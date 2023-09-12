2ce49c1cbd90d3579cad28808bf8770ea8a55bdd
Git-Seokcoding <alstjr3824@naver.com>
2023-09-12 15:12:37 +0900

    9/12

--- JAVA0912/src/access_modifier/test.java
+++ JAVA0912/src/access_modifier/test.java
@@ -2,41 +2,49 @@ package access_modifier;
 
 public class test {
 	
-	/*
-	 *Account 클래스 정의
-	 *- 멤버변수 (private 접근제한자로 선언)
-	 *	accountNo(문자열), ownerName(문자열), balance(정수)
-	 *- Getter/Setter 메서드 정의(단축키 사용 가능) 
-	 */
-
-		private String accountNo;
-		private String ownerName;
-		private int balance;
-		
-		//-------------------------------------------------------------------
-		public String getAccountNo() {
-			return accountNo;
-		}
-		public void setAccountNo(String accountNo) {
-			this.accountNo = accountNo;
-		}
-		//-------------------------------------------------------------------
-		public String getOwnerName() {
-			return ownerName;
-		}
-		public void setOwnerName(String ownerName) {
-			this.ownerName = ownerName;
-		}
-		//-------------------------------------------------------------------
-		public int getBalance() {
-			return balance;
-		}
-		public void setBalance(int balance) {
-			this.balance = balance;
-		}
+	public static void main(String[] args) {
 		
 		
+	}
 
 }
 
+/*
+ *Account 클래스 정의
+ *- 멤버변수 (private 접근제한자로 선언)
+ *	accountNo(문자열), ownerName(문자열), balance(정수)
+ *- Getter/Setter 메서드 정의(단축키 사용 가능) 
+ */
 
+class Account{
+	// Account 클레스의 멤버변수 private 접근제한자로 선언
+	private String accountNo;
+	private String ownerName;
+	private int balance;
+	
+	//-------------------------------------------------------------------
+	// 1. String owerName 변수에 대한 Getter/Setter
+	public String getAccountNo() {
+		return accountNo;
+	}
+	public void setAccountNo(String newAccountNo) {
+		this.accountNo = newAccountNo;
+	}
+	//-------------------------------------------------------------------
+	// 2. String owerName 변수에 대한 Getter/Setter
+	public String getOwnerName() {
+		return ownerName;
+	}
+	public void setOwnerName(String newOwnerName) {
+		this.ownerName = newOwnerName;
+	}
+	//-------------------------------------------------------------------
+	// 3. int balance 변수에 대한 Getter/Setter
+
+	public int getBalance() {
+		return balance;
+	}
+	public void setBalance(int newBalance) {
+		this.balance = newBalance;
+	}
+}
