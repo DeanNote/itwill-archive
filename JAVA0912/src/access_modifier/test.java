4ef4999cd269a72ac53fe89a60ce92c607964da3
Git-Seokcoding <alstjr3824@naver.com>
2023-09-12 15:07:12 +0900

    9/12

--- JAVA0912/src/access_modifier/test.java
+++ JAVA0912/src/access_modifier/test.java
@@ -1,6 +1,14 @@
 package access_modifier;
 
 public class test {
+	
+	/*
+	 *Account 클래스 정의
+	 *- 멤버변수 (private 접근제한자로 선언)
+	 *	accountNo(문자열), ownerName(문자열), balance(정수)
+	 *- Getter/Setter 메서드 정의(단축키 사용 가능) 
+	 */
+
 		private String accountNo;
 		private String ownerName;
 		private int balance;
@@ -31,10 +39,4 @@ public class test {
 
 }
 
-/*
- *Account 클래스 정의
- *- 멤버변수 (private 접근제한자로 선언)
- *	accountNo(문자열), ownerName(문자열), balance(정수)
- *- Getter/Setter 메서드 정의(단축키 사용 가능) 
- */
 
