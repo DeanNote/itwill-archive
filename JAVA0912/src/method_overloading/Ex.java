0b24e0f74480437db3ba7353ef85cb1690c18d11
Git-Seokcoding <alstjr3824@naver.com>
2023-09-12 15:35:48 +0900

    9/12

--- JAVA0912/src/method_overloading/Ex.java
+++ JAVA0912/src/method_overloading/Ex.java
@@ -19,8 +19,31 @@ public class Ex {
 		 *   2. 메서드 파라미터의 개수가 달라야 함
 		 *   
 		 */
-		
-		
+		System.out.println();
 	}
 
+}
+
+class NomalMethod{
+	//두 수를 전달받아 덧셈 결과를 출력하는 메서드 정의
+	//1. int형 정수 2개를 전달 받았을 경우
+	public void addInt(int num1, int num2) {
+		System.out.println(num1+num2);
+		
+	}
+	//2. double형 실수 2개를 전달받을 경우
+	public void addDouble(double num1, double num2) {
+		System.out.println(num1+num2);
+	}
+	//일반적으로 메서드명도 식별자에 해당되므로 중복이 불가능함
+	//따라서, addInt()메서드와 addDouble()메서드는 하는 일은 같지만 
+	//서로 다른 파라미터를 전달받으므로 다른 이름으로 정의했음.
+	//=>메서드 정의 시 메서드명을 달리해야하므로 호출 시 구분이 불편할 구 있음.
+	public void printInt(int num) {
+		System.out.println(num);
+	}
+	
+	public void printString(String str) {
+		System.out.println(str);
+	}
 }
