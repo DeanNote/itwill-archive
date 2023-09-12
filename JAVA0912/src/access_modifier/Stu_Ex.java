faf5afbaae25c262a02efe567d9ff68a1e35fbb2
Git-Seokcoding <alstjr3824@naver.com>
2023-09-12 14:06:15 +0900

    9/12

--- JAVA0912/src/access_modifier/Stu_Ex.java
+++ JAVA0912/src/access_modifier/Stu_Ex.java
@@ -18,6 +18,8 @@ public class Stu_Ex {
 		//Person 클래스의 인스턴스(p) 생성
 		Person p = new Person();
 		
+		// 멤버변수 값을 변경하기 위해 참조변수를 통해 멤버 변수에 접근
+		//private 접근제한자 때문에 외부 클래스에저 접근 불가!
 		p.name ="홍길동"; //private 접근제한자 때문에 name 값 수정 못함
 		p.age = 20;
 		
