55bf5a3a744ebfe27139460485295616faa8a101
Git-Seokcoding <alstjr3824@naver.com>
2023-09-12 14:04:02 +0900

    9/12(화)

--- JAVA0912/src/access_modifier/Stu_Ex.java
+++ JAVA0912/src/access_modifier/Stu_Ex.java
@@ -3,11 +3,23 @@ package access_modifier;
 public class Stu_Ex {
 
 	public static void main(String[] args) {
+		/*
+		 * 1. private 접근제한자
+		 * 외부 클래스에서 접근x 자신의 클래스 내에서만 가능
+		 * 주로 외부에서 접근못하게 멤버변수에 적용
+		 * 
+		 * 2. public 접근제한자
+		 * 누구나 접근가능
+		 * 주로 외부에서 접근하는 메서드에 적용
+		 * 
+		 * 
+		 * 
+		 */
 		//Person 클래스의 인스턴스(p) 생성
 		Person p = new Person();
 		
 		p.name ="홍길동"; //private 접근제한자 때문에 name 값 수정 못함
-		int age = 20;
+		p.age = 20;
 		
 	}
 
