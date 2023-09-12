cee05b374723c2100a593f5bb0624e961d48e98c
Git-Seokcoding <alstjr3824@naver.com>
2023-09-12 14:00:34 +0900

    9/12(화)

--- JAVA0912/src/access_modifier/Stu_Ex.java
+++ JAVA0912/src/access_modifier/Stu_Ex.java
@@ -6,7 +6,7 @@ public class Stu_Ex {
 		//Person 클래스의 인스턴스(p) 생성
 		Person p = new Person();
 		
-		p.name ="홍길동"; //private
+		p.name ="홍길동"; //private 접근제한자 때문에 name 값 수정 못함
 		int age = 20;
 		
 	}
