e52a987125a2465b70adea4342f9624722728d5c
Git-Seokcoding <alstjr3824@naver.com>
2023-09-12 15:18:00 +0900

    9/12

--- JAVA0912/src/access_modifier/test.java
+++ JAVA0912/src/access_modifier/test.java
@@ -3,6 +3,27 @@ package access_modifier;
 public class test {
 	
 	public static void main(String[] args) {
+		// 은행계좌(Account) 클래스의 인스턴스(acc) 생성
+		Account acc = new Account();
+		
+//		acc.accountNO = "1111-2222-333";
+		//접근제한자가 private로 설정되어 있어 접근 불가!
+		
+		//Setter()메서드로 "1111-2222-333","홍길동",1000 입력
+		acc.setAccountNo("1111-2222-333");
+		acc.setOwnerName("홍길동");
+		acc.setBalance(1000);
+		
+//		System.out.println("계좌번호 : " + acc.accountNo());
+//		System.out.println("예금주명 : " + acc.ownerName());
+//		System.out.println("현재잔고 : " + acc.balance());
+		// => Getter()메서드로 대체
+		
+		// => Getter()메서드를 활용하여 계좌번호, 예금주명, 현재잔고를 출력
+		System.out.println("계좌번호 : " + acc.getAccountNo());
+		System.out.println("예금주명 : " + acc.getOwnerName());
+		System.out.println("현재잔고 : " + acc.getBalance());
+		
 		
 		
 	}
