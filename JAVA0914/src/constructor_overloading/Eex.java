c0f3be612c513d79779ddae1a4f144453e484fd0
Git-Seokcoding <alstjr3824@naver.com>
2023-09-14 16:40:03 +0900

    9/14

--- JAVA0914/src/constructor_overloading/Eex.java
+++ JAVA0914/src/constructor_overloading/Eex.java
@@ -27,7 +27,9 @@ class Person{
 	// 파라미터 2개 (name, jumin)를 전달받아 초기화 하는 생성자 정의
 	public Person(String newName, String newJumin) {
 		System.out.println("Person(String, String) 생성자 호출됨!");
-		System.out.println("Person()생성자 호출됨!");
-		System.out.println("Person()생성자 호출됨!");
+		nation = "대한민국";
+		name = newName;
+		jumin = newJumin;
+		
 	}
 }
