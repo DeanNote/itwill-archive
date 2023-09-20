0ef782422e0a4e72bd3e93fcacb29f232ef71b86
Git-Seokcoding <alstjr3824@naver.com>
2023-09-20 16:14:54 +0900

    9/20 this

---
+++ JAVA0920/src/this_/Stu_Ex.java
@@ -0,0 +1,16 @@
+package this_;
+
+public class Stu_Ex {
+
+	public static void main(String[] args) {
+		/*
+		 * this 키워드
+		 * - 자신의 인스턴스 주소값을 저장하는 참조 변수
+		 *   => 개발자가 생성하는 것이 아니라 자바에 의해 자동으로 생성됨
+		 * - 모든 인스턴스 내에는 this 가 존재하며, 자신의 인스턴스 주소가 저장됨
+		 *   => 즉, 인스턴스 마다 this 에 저장된 값이 다름
+		 * 
+		 */
+	}
+
+}
