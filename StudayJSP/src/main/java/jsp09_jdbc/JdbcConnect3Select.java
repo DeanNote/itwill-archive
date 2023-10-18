8291c0fb43ebf78e1d3cc3e23e6a1e26356a6046
Git-Seokcoding <alstjr3824@naver.com>
2023-10-18 10:10:21 +0900

    10/18 JDBC

---
+++ StudayJSP/src/main/java/jsp09_jdbc/JdbcConnect3Select.java
@@ -0,0 +1,19 @@
+package jsp09_jdbc;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/JdbcConnect3_SELECT")
+public class JdbcConnect3Select extends HttpServlet {
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		System.out.println("/JdbcConnect3_INSERT");
+		
+		
+		
+	}
+		
+}
---
+++ StudayJSP/src/main/java/jsp09_jdbc/JdbcConnect3Update.java
@@ -0,0 +1,19 @@
+package jsp09_jdbc;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/JdbcConnect3_UPDATE")
+public class JdbcConnect3Update extends HttpServlet {
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		System.out.println("/JdbcConnect3_INSERT");
+		
+		
+		
+	}
+		
+}
