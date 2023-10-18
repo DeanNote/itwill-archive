e1b5c5bced14dd00554a4bbf7d89a3ad8e430d4a
Git-Seokcoding <alstjr3824@naver.com>
2023-10-18 17:13:42 +0900

    10/18 JDBC

--- StudayJSP/src/main/java/jsp09_jdbc/JdbcConnect3Delete.java
+++ StudayJSP/src/main/java/jsp09_jdbc/JdbcConnect3Delete.java
@@ -16,11 +16,26 @@ public class JdbcConnect3Delete extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		System.out.println("JdbcConnect3Delete");
 		
-		Class.forName("com.mysql.cj.jdbc.Driver");
-		System.out.println("드라이버 로드 성공!");
-		DriverManager.getConnection("jdbc:mysql://localhost:3306/study_jsp5", "root", "1234");
-		System.out.println("DB 연결 성공!");
-
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			System.out.println("드라이버 로드 성공!");
+			
+			DriverManager.getConnection("jdbc:mysql://localhost:3306/study_jsp5", "root", "1234");
+			System.out.println("DB 연결 성공!");
+			
+			String sql = "INSERT INTO jsp09_student VALUES (?, ?)";
+			
+			pstmt = con.prepareStatement(sql);
+					pstmt.setInt(1, idx); 
+			pstmt.setString(2, name);
+			
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		
 		
 	}
