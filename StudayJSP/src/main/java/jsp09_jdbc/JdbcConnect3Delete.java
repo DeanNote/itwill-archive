e282b02896a04f8e0239b8da40557279940fc0ff
Git-Seokcoding <alstjr3824@naver.com>
2023-10-18 13:57:24 +0900

    10/18 JDBC

--- StudayJSP/src/main/java/jsp09_jdbc/JdbcConnect3Delete.java
+++ StudayJSP/src/main/java/jsp09_jdbc/JdbcConnect3Delete.java
@@ -16,6 +16,11 @@ public class JdbcConnect3Delete extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		System.out.println("JdbcConnect3Delete");
 		
+		Class.forName("com.mysql.cj.jdbc.Driver");
+		System.out.println("드라이버 로드 성공!");
+		DriverManager.getConnection("jdbc:mysql://localhost:3306/study_jsp5", "root", "1234");
+		System.out.println("DB 연결 성공!");
+
 		
 		
 	}
