102d7e7446b715d2296df22d7fcb89b2ad519fb8
Git-Seokcoding <alstjr3824@naver.com>
2023-10-18 17:13:57 +0900

    10/18 JDBC

--- StudayJSP/src/main/java/jsp09_jdbc/JdbcConnect3Delete.java
+++ StudayJSP/src/main/java/jsp09_jdbc/JdbcConnect3Delete.java
@@ -27,6 +27,7 @@ public class JdbcConnect3Delete extends HttpServlet {
 			
 			pstmt = con.prepareStatement(sql);
 					pstmt.setInt(1, idx); 
+					
 			pstmt.setString(2, name);
 			
 		} catch (ClassNotFoundException e) {
