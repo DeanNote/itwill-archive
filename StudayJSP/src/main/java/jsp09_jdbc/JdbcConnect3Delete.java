745191418f9ebc14b6b035f6f03f727f16c0552b
Git-Seokcoding <alstjr3824@naver.com>
2023-10-18 17:15:23 +0900

    10/18 JDBC

--- StudayJSP/src/main/java/jsp09_jdbc/JdbcConnect3Delete.java
+++ StudayJSP/src/main/java/jsp09_jdbc/JdbcConnect3Delete.java
@@ -25,7 +25,7 @@ public class JdbcConnect3Delete extends HttpServlet {
 			
 			String sql = "INSERT INTO jsp09_student VALUES (?, ?)";
 			
-			pstmt = con.prepareStatement(sql);
+//			pstmt = con.prepareStatement(sql);
 //					pstmt.setInt(1, idx); 
 //			pstmt.setString(2, name);
 			
