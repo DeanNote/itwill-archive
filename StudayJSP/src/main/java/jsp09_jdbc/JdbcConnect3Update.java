ee34ffb679c4b94d8fa8f45edb1db8bfb6cabd72
Git-Seokcoding <alstjr3824@naver.com>
2023-10-20 10:53:53 +0900

    10/20 jdbc Update

--- StudayJSP/src/main/java/jsp09_jdbc/JdbcConnect3Update.java
+++ StudayJSP/src/main/java/jsp09_jdbc/JdbcConnect3Update.java
@@ -18,6 +18,8 @@ public class JdbcConnect3Update extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		System.out.println("JdbcConnect3Update");
 		
+		// DB 자원을 관리하는 Connection, PreparedStatement 등의 타입 변수 선언
+		// => finally 블럭에서 접근하기 위함
 		Connection con = null;
 		PreparedStatement pstmt = null;
 		
@@ -33,54 +35,39 @@ public class JdbcConnect3Update extends HttpServlet {
 			System.out.println("드라이버 로드 성공!");
 			
 			// 2단계. DB 연결
-			// => DB 연결 성공 시 java.sql.Connection 타입 객체 리턴됨
-			// => Connection 객체는 DB 접속 정보를 관리하는 객체
 			con = DriverManager.getConnection(url, user, password);
 			System.out.println("DB 연결 성공!");
 			
+			// 3단계. SQL 구문 작성 및 전달
+			// 번호가 3 인 레코드의 이름을 '조인성' 으로 변경
+			int idx = 3;
+			String name = "조인성";
 			
-			int idx = 3; // int idx = Integer.parseInt(request.getParameter("idx"));
-			String name = "조인성"; // String name = request.getParameter("name");
-			
-			String sql = "UPDATE jsp09_student SET name = ? WHERE idx = ? ";
+			String sql = "UPDATE jsp09_student SET name = ? WHERE idx = ?";
 			pstmt = con.prepareStatement(sql);
 			pstmt.setString(1, name);
 			pstmt.setInt(2, idx);
 			
-		
 			System.out.println(pstmt);
-			// --------------------
+			
 			// 4단계. SQL 구문 실행 및 결과 처리
-			// - INSERT 구문이므로 PreparedStatement 객체의 executeUpdate() 메서드 호출
-			//   => 파라미터 : 없음   리턴타입 : int
 			int updateCount = pstmt.executeUpdate();
-			
-			// 주의! 3단계 과정에서 SQL 구문을 메서드 파라미터로 전달한 것과 상관없이
-			// 4단계 메서드 호출 시 SQL 구문을 다시 전달할 경우
-			// setXXX() 메서드를 활용한 데이터 치환이 수행되지 않은 문장이 실행되므로 오류!
-//			int insertCount = pstmt.executeUpdate(sql);
-			
-			System.out.println("회원 수정(update) 성공 - " + updateCount);
-			
-			
+			System.out.println("수정 성공! - " + updateCount);
 		} catch (ClassNotFoundException e) {
-			// 1단계 - 드라이버 로드 작업 실패 시 실행되는 코드들...
 			System.out.println("드라이버 로드 실패!");
 			e.printStackTrace();
 		} catch (SQLException e) {
-			// 2단계 - DB 연결 작업 실패 시 실행되는 코드들...
-			// 3단계, 4단계 - SQL 구문 작업 실패 시에도 실행되는 코드들...
-			System.out.println("DB 연결 실패! 또는 SQL 구문 오류 발생!");
+			System.out.println("DB 연결 실패! 또는 SQL 구문 오류!");
 			e.printStackTrace();
 		} finally {
 			try {
-				pstmt.close(); // PreparedStatemente 객체 반납
-				con.close(); // Connection 객체 반납(닫기 = 자원 해제)
+				// DB 자원 반환(Connection, PreparedStatement)
+				pstmt.close();
+				con.close();
 			} catch (SQLException e) {
 				e.printStackTrace();
 			}
 		}
-		
 		
 	}
 
