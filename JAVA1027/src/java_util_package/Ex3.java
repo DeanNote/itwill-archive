7eced8794b6bc3ea3fef8ef4cf509150f3d80721
Seokcoding <142768477+Git-Seokcoding@users.noreply.github.com>
2023-10-27 14:01:32 +0900

    10/27

--- JAVA1027/src/java_util_package/Ex3.java
+++ JAVA1027/src/java_util_package/Ex3.java
@@ -1,9 +1,19 @@
 package java_util_package;
 
+import java.util.Date;
+
 public class Ex3 {
 
 	public static void main(String[] args) {
-
+		/*
+		 * [ 날짜 및 시각 관련 클래스 ]
+		 * 1. Date클래스
+		 * - toString() 메서드가 오버라이딩 되어 있음
+		 * 
+		 */
+		Date d1 = new Date();
+		System.out.println(d1);
+		
 	}
 
 }
