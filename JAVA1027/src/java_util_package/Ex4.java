6a44ad77bc8d4ffd6965181bbbff28cfabb9f5a6
Seokcoding <142768477+Git-Seokcoding@users.noreply.github.com>
2023-10-27 14:25:29 +0900

    10/27

--- JAVA1027/src/java_util_package/Ex4.java
+++ JAVA1027/src/java_util_package/Ex4.java
@@ -1,6 +1,7 @@
 package java_util_package;
 
 import java.util.Calendar;
+import java.util.Date;
 
 public class Ex4 {
 
@@ -17,9 +18,51 @@ public class Ex4 {
 		 * 
 		 */
 		
-		Calendar cal = new Calendar();
+//		Calendar cal = new Calendar();
 		// => Cannot instantiate the type Calendar
 		// => 추상클래스 이므로 인스턴스 생성 불가!
+		Calendar cal = Calendar.getInstance();
+		// 현재 시스템 날짜 및 시각 정보 리턴
+		
+		// toString()메서드가 오버라이딩 되어 있으나 식별이 어려움
+//		System.out.println(cal);
+		int year = cal.get(Calendar.YEAR);
+		int month = cal.get(Calendar.MONTH);
+		int day = cal.get(Calendar.DAY_OF_MONTH);
+		System.out.print(year + "년");
+		System.out.print((month+1) + "월");
+		System.out.println(day + "일");
+		
+		int hour = cal.get(Calendar.HOUR_OF_DAY);//24시간제(HOUR : 12시간제)
+		int min = cal.get(Calendar.MINUTE);
+		int sec = cal.get(Calendar.SECOND);
+		System.out.println(hour + ":" + min + ":" + sec);
+		
+		System.out.println("----------------------------------------");
+		
+		/*
+		 Calendar <-> Date 상호 변환
+		1. Calendar -> Date로 변환 시
+		*/
+		Date date = cal.getTime();
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
 		
 		
 	}
