4ff37a98c27faa4e6b6140b36f17aa74670c3024
Seokcoding <142768477+Git-Seokcoding@users.noreply.github.com>
2023-10-30 14:05:31 +0900

    10/30

--- JAVA1030/src/generic/Ex.java
+++ JAVA1030/src/generic/Ex.java
@@ -4,12 +4,42 @@ public class Ex {
 
 	public static void main(String[] args) {
 
-	}
+		NormalIntegetClass nic = new NormalIntegetClass();
+		
+		nic.data = 10;
+//		nic.data = 3.14;
+//		nic.data = "홍길동";
+		
+		NormalObjectClass noc = new NormalObjectClass();
+		
+		noc.data = 10;
+		noc.data = 3.14;
+		noc.data = "홍길동";
+		
+	}//메인 끝
 
-}
+}//EX 끝
 
 class NormalIntegetClass{
 	int data;// data 변수에는 정수형 데이터만 저장 가능함
-	
-	
+
+	public int getData() {
+		return data;
+	}
+
+	public void setData(int data) {
+		this.data = data;
+	}
+}
+
+class NormalObjectClass{
+	Object data;
+
+	public Object getData() {
+		return data;
+	}
+
+	public void setData(Object data) {
+		this.data = data;
+	}
 }
