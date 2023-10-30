dbad0d0eacc633b55e0adfb826c2b328e83cb2e4
Seokcoding <142768477+Git-Seokcoding@users.noreply.github.com>
2023-10-30 16:35:07 +0900

    10/30

--- JAVA1030/src/collection_framework/Ex.java
+++ JAVA1030/src/collection_framework/Ex.java
@@ -3,8 +3,12 @@ package collection_framework;
 public class Ex {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		/*
+		 Collection Framework(컬렉션 프레임워크)
+		 - 컴퓨터 시스템에서 데이터를 효율적으로 저장 및 관리하는 방법
+		   = 자료구조(Data Structure)라고 함
+		   => 자료구조를 자바에서 구현하여 제공하는 클래스들의 모음(API)
+		 */
 	}
 
 }
