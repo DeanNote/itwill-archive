8db635a852a1fe51551c4291b1e1f0a598285a4a
Seokcoding <142768477+Git-Seokcoding@users.noreply.github.com>
2023-10-30 11:36:00 +0900

    10/30

--- StudyJSP/src/main/java/jsp14_servlet2/BoardServlet.java
+++ StudyJSP/src/main/java/jsp14_servlet2/BoardServlet.java
@@ -1,6 +1,8 @@
 package jsp14_servlet2;
 
 import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -15,11 +17,24 @@ public class BoardServlet extends HttpServlet {
 	}
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		request.setCharacterEncoding("UTF-8");
 		doProcess(request, response);
 	}
 
 	protected void doProcess(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		System.out.println("BoardServlet - *.bo 주소 매핑");
+		
+		String command = request.getServletPath();
+		if(command.equals("/jsp14_servlet2/BoardWriteForm.bo")) {
+			RequestDispatcher dispatcher = request.getRequestDispatcher("/jsp14_servlet2/board_write_form.jsp");
+			dispatcher.forward(request, response);
+			
+		}else if(command.equals("/jsp14_servlet2/BoardList.bo")) {
+			RequestDispatcher dispatcher = request.getRequestDispatcher("/jsp14_servlet2/board_list.jsp");
+			dispatcher.forward(request, response);
+		}
+		
+		
 	}
 }
 
