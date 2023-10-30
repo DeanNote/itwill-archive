676c517aca105360ca537fe3bd898bc16e8dec53
Seokcoding <142768477+Git-Seokcoding@users.noreply.github.com>
2023-10-30 17:00:42 +0900

    10/30

--- JAVA1030/src/collection_framework/Ex.java
+++ JAVA1030/src/collection_framework/Ex.java
@@ -1,5 +1,8 @@
 package collection_framework;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class Ex {
 
 	public static void main(String[] args) {
@@ -8,7 +11,60 @@ public class Ex {
 		 - 컴퓨터 시스템에서 데이터를 효율적으로 저장 및 관리하는 방법
 		   = 자료구조(Data Structure)라고 함
 		   => 자료구조를 자바에서 구현하여 제공하는 클래스들의 모음(API)
+		 - Set, List, Map 구분
+		   => Set과 List는 공통적으로 Collection 인터페이스 상속받으므로
+		      제공되는 메서드가 거의 동일함
+		 - 각 인터페이스들의 구현체 클래스를 사용하여 데이터를 관리함
+		 - java.util 패키지에 컬렉션 프레임워크 API 들이 제공됨
+		 - toString()메서드가 오버라이딩 되어있음
+		 - 모든 데이터는 객체 형태로 관리되며, 기본 데이터타입사용 시
+		 	Wrapper 클래스 타입 오토 박싱 일어남
+
+		 	1. Set 계열
+		 	- 저장 순서 유지 X, 데이터 중복 허용 X
+				=> 주머니에 공을 무작위로 집어넣는 것과 유사한 구조로
+				   데이터의 저장되는 순서 유지 및 중복이 불가능한 구조
+				   => 매우 효율적인 중복 제거 수단으로 사용 가능
+		    - 인덱스를 사용하지 않음
+		    - 구현체 클래스 : HashSet, TreeSet 등
+		    
 		 */
+		
+		
+		// HashSet 객체
+//		HashSet set = new HashSet();
+		Set set = new HashSet();
+		
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 	}
 
 }
