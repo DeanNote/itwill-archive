aca415e754638f68d65e40e0ddd0fd03dbb6551d
Seokcoding <142768477+Git-Seokcoding@users.noreply.github.com>
2023-10-31 17:04:37 +0900

    10/31

--- JAVA1031/src/collection_framework/Ex4.java
+++ JAVA1031/src/collection_framework/Ex4.java
@@ -1,5 +1,8 @@
 package collection_framework;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Ex4 {
 
 	public static void main(String[] args) {
@@ -8,8 +11,13 @@ public class Ex4 {
 		 - key 와 value 한쌍 갖는 자료구조
 		   Map.Entry 타입 객체로 관리됨
 		 - 키는 중복 불가, 값은 중복 가능
-		 
+		 - 대표적인 구현체 클래스 : HashSet, Properties
 		 */
+		
+		Map map = new HashMap();
+		
+		
+		
 	}
 
 }
