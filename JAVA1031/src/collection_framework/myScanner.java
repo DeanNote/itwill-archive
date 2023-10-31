3589ee6ebdf722f642a856e1bdc861d9ea8e1481
Seokcoding <142768477+Git-Seokcoding@users.noreply.github.com>
2023-10-31 15:34:27 +0900

    10/31

--- JAVA1031/src/collection_framework/myScanner.java
+++ JAVA1031/src/collection_framework/myScanner.java
@@ -6,16 +6,11 @@ public class myScanner
 {
     public static void main(String[] args)
     {
-        Scanner scanner = new Scanner(System.in);
- 
-        int i = 0;
- 
-        while(scanner.hasNextInt())
-        {
-            int num = scanner.nextInt();
-            i += num;
-        }
- 
-        System.out.println("total : " + i);
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        System.out.printf("%d\n%d\n%d\n%d\n",a+b,a-b,a*b,a/b);
+        
+       
     }
 }
