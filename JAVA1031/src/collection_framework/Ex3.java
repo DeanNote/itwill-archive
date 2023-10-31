5a81ea7c496cc568e8ef24c6cac0eddf20ba8309
Seokcoding <142768477+Git-Seokcoding@users.noreply.github.com>
2023-10-31 16:19:13 +0900

    10/31

--- JAVA1031/src/collection_framework/Ex3.java
+++ JAVA1031/src/collection_framework/Ex3.java
@@ -16,6 +16,16 @@ public class Ex3 {
 		list.add(2);
 		list.add(3);
 //		list.add(3.14); // 제네릭 타입 고정!
+		
+		System.out.println("list 객체가 비어있는가? " + list.isEmpty());
+		System.out.println("list 객체에 저장된 요소 개수 : " + list.size());
+		System.out.println("list 객체에 저장된 모든 요소 : " + list);
+		
+		System.out.println("중복 데이터 3 추가 가능한가? : " + list.add(3));
+		System.out.println("list 객체에 저장된 모든 요소 : " + list);
+		
+		// Object get(int index) : index에 해당하는 요소 리턴
+		
 	}
 
 }
