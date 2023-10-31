cd6bdf82dd5000791976fac62453627f733b8e9b
Seokcoding <142768477+Git-Seokcoding@users.noreply.github.com>
2023-10-31 16:02:21 +0900

    10/31

---
+++ JAVA1031/src/collection_framework/Ex2.java
@@ -0,0 +1,22 @@
+package collection_framework;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class Ex2 {
+
+	public static void main(String[] args) {
+		/*
+		 반복문 사용
+		 */
+		
+		Set set = new HashSet();
+		set.add(300);
+		set.add(30);
+		set.add(200);
+		set.add(1);
+		set.add(100);
+		set.add(10);
+	}
+
+}
