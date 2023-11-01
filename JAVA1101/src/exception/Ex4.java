5bc68361593e4537a5419a5616cdf12b0591e389
Seokcoding <142768477+Git-Seokcoding@users.noreply.github.com>
2023-11-01 17:27:02 +0900

    11/01

--- JAVA1101/src/exception/Ex4.java
+++ JAVA1101/src/exception/Ex4.java
@@ -6,12 +6,33 @@ public class Ex4 {
 		/*
 		 사용자에 의한 예외 발생(throw)
 		 */
+		
+		try {
+			printScore(111);
+		} catch (Exception e) {
+			e.printStackTrace();
+			System.out.println(e.getMessage());
+		}
 	}//main end
 
-	public static void printScor(int Score) {
+	public static void printScore(int Score) throws Exception {
 		if(Score < 0 || Score > 100) {
-			System.out.println(Score + "점 : 점수 입력 오류!");
+//			System.out.println(Score + "점 : 점수 입력 오류!");
+			//throw 키워드를 사용
+//			Exception e = new Exception("점수 입력 오류 - " + Score);
+//			throw e;
+			throw new Exception("점수 입력 오류 - " + Score);
 		}
 	}
 	
 }//Ex4 end
+
+/*
+사용자 정의 예외 클래스 작성
+*/
+ 
+class InvalidScoreException extends Exception {
+	public InvalidScoreException(String message) {
+		super(message);
+	}
+}
