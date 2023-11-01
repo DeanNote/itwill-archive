be6f394d6e6a5ceb36f0d67df459871272950d0a
Seokcoding <142768477+Git-Seokcoding@users.noreply.github.com>
2023-11-01 09:11:07 +0900

    11/01

--- MVC_Board/src/main/java/vo/MemberBean.java
+++ MVC_Board/src/main/java/vo/MemberBean.java
@@ -3,23 +3,44 @@ package vo;
 import java.sql.Date;
 
 /*
-CREATE TABLE member(
- idx  int PRIMARY KEY AUTO_INCREMENT,
- name varchar(10) NOT NULL,
- id varchar(16) UNIQUE NOT NULL,
- passwd varchar(16) NOT NULL,
- jumin varchar(14) UNIQUE NOT NULL,
- address varchar(100) NOT NULL,
- email varchar(50) NOT NULL UNIQUE,
- job varchar(10) NOT NULL,
- gender varchar(1) NOT NULL,
- hobby varchar(50) NOT NULL,
- motivation varchar(500) NOT NULL,
- reg_date DATETIME NOT NULL
- );
-
- */
+[ mvc_board5.member 테이블 정의 ]
+----------------------------------------
+번호(idx) - 정수, PK, 자동증가(AUTO_INCREMENT)
+이름(name) - 문자(10자), NN
+아이디(id) - 문자(16자), UN, NN
+패스워드(passwd) - 문자(16자), NN
+주민번호(jumin) - 문자(14자), UN, NN
+주소(address) - 문자(100자), NN
+이메일(email) - 문자(50자), UN, NN
+직업(job) - 문자(10자), NN
+성별(gender) - 문자(1자), NN
+취미(hobby) - 문자(50자), NN
+가입동기(motivation) - 문자(500자), NN
+가입일(reg_date) - 날짜(DATETIME), NN
+-----------------------------------------
+CREATE DATABASE mvc_board5;
+USE mvc_board5;
+CREATE TABLE member (
+	idx INT PRIMARY KEY AUTO_INCREMENT,
+ 	name VARCHAR(10) NOT NULL,
+ 	id VARCHAR(16) NOT NULL UNIQUE,
+ 	passwd VARCHAR(16) NOT NULL,
+ 	jumin VARCHAR(14) NOT NULL UNIQUE,
+ 	address VARCHAR(100) NOT NULL,
+ 	email VARCHAR(50) NOT NULL UNIQUE,
+ 	job VARCHAR(10) NOT NULL,
+ 	gender VARCHAR(1) NOT NULL,
+ 	hobby VARCHAR(50) NOT NULL,
+ 	motivation VARCHAR(500) NOT NULL,
+ 	reg_date DATETIME NOT NULL  => java.sql.Date 클래스 활용
+);
+*/
+// member 테이블에 대응하는 MemberBean 클래스 정의(= DTO 클래스 역할)
 public class MemberBean {
+	// 1. 멤버변수 선언
+	// --- 생성자 정의 생략(기본 생성자 활용) ---
+	// 2. Getter/Setter 정의
+	// 3. toString() 메서드 오버라이딩
 	private int idx;
 	private String name;
 	private String id;
@@ -31,15 +52,7 @@ public class MemberBean {
 	private String gender;
 	private String hobby;
 	private String motivation;
-	private Date reg_date;
-	
-	@Override
-	public String toString() {
-		return "MemberBean [idx=" + idx + ", name=" + name + ", id=" + id + ", passwd=" + passwd + ", jumin=" + jumin
-				+ ", address=" + address + ", email=" + email + ", job=" + job + ", gender=" + gender + ", hobby="
-				+ hobby + ", motivation=" + motivation + ", reg_date=" + reg_date + ", toString()=" + super.toString()
-				+ "]";
-	}
+	private Date reg_date; // java.sql.Date
 	
 	public int getIdx() {
 		return idx;
@@ -113,6 +126,24 @@ public class MemberBean {
 	public void setReg_date(Date reg_date) {
 		this.reg_date = reg_date;
 	}
+	@Override
+	public String toString() {
+		return "MemberBean [idx=" + idx + ", name=" + name + ", id=" + id + ", passwd=" + passwd + ", jumin=" + jumin
+				+ ", address=" + address + ", email=" + email + ", job=" + job + ", gender=" + gender + ", hobby="
+				+ hobby + ", motivation=" + motivation + ", reg_date=" + reg_date + "]";
+	}
 	
 	
 }
+
+
+
+
+
+
+
+
+
+
+
+
