6b5941ecbe9d95ae6d5571cb0eeb032fb2503c13
Seokcoding <142768477+Git-Seokcoding@users.noreply.github.com>
2023-11-03 10:45:47 +0900

    11/03

--- MVC_Board/src/main/java/svc/MemberJoinProService.java
+++ MVC_Board/src/main/java/svc/MemberJoinProService.java
@@ -2,6 +2,7 @@ package svc;
 
 import java.sql.Connection;
 
+import dao.MemberDAO;
 import db.JdbcUtil;
 import vo.MemberBean;
 
@@ -14,9 +15,12 @@ public class MemberJoinProService {
 		// 1. 작업 요청 처리 결과 저장할 변수 선언
 		boolean isJoinSuccess = false;
 		
-		// 2. db.JdbcUtill 클래스로부터 커넥션 풀에 저장된 커넥션 객체 가져오기
+		// 2. db.JdbcUtill 클래스로부터 커넥션 풀에 저장된 커넥션 객체 가져오기(공통)
 		// => 트랜잭션 관리를 DAO 대신 서비스 객체가 수행해야 하므로 Service 객체가 Connection 객체를 관리해야함.
 		Connection con = JdbcUtil.getConnection();
+		
+		// 3. MemberDAO
+		MemberDAO dao = new MemberDAO();
 		
 		
 		//작업 요청 처리 결과 리턴
