e16e6deacb6f5240137497451d82efb4e233e3e1
Seokcoding <142768477+Git-Seokcoding@users.noreply.github.com>
2023-11-13 13:36:16 +0900

    11/13

--- MVC_Board/src/main/java/action/BoardListAction.java
+++ MVC_Board/src/main/java/action/BoardListAction.java
@@ -30,56 +30,63 @@ public class BoardListAction implements Action {
 		System.out.println("현재 페이지 : " + pageNum);
 		
 		// 한 페이지에서 표시할 글 목록 갯수 지정
-		int listLimit = 5;
+		int listLimit = 10;
+		
 		// 조회 시작 행(레코드) 번호 계산
+		// => 0번부터 시작하여 페이지 당 목록 갯수씩 증가해야함
 		int startRow = (pageNum - 1) * listLimit;
 		// --------------------------------------------------------------------
 		// BoardListService - getBoardList() 메서드 호출하여 글목록 조회 요청
 		// => 파라미터 : 시작행번호, 목록갯수   리턴타입 : java.util.List<BoardBean>(boardList)
 		BoardListService service = new BoardListService();
 		List<BoardBean> boardList = service.getBoardList(startRow, listLimit);
-		System.out.println(boardList);
-		
+//		System.out.println(boardList);
 		// --------------------------------------------------------------------
 		// JSP 페이지에서의 페이징 처리
 		// 한 페이지에서 표시할 페이지 목록(번호) 계산
 		// 1) BoardListService - getBoardListCount() 메서드를 호출하여
-		//	전체 게시물 수 조회(페이지 목록 계산에 활용)
-		//  파라미터 없음, 리턴 int
-		
+		//    전체 게시물 수 조회(페이지 목록 계산에 활용)
+		// => 파라미터 : 없음   리턴타입 : int(listCount)
 		int listCount = service.getBoardListCount();
-		System.out.println("전체 게시물 수 : " + listCount);
+//		System.out.println("전체 게시물 수 : " + listCount);
 		
-		// 2) 한페이지에서 표시할 페이지 목록갯수
-		int pageListLimit = 3; //임시) 페이지 당 페이지 번호 갯수를 2개로 지정
+		// 2) 한 페이지에서 표시할 페이지 목록 갯수(페이지 번호 갯수) 설정
+		int pageListLimit = 3; // 임시) 페이지 당 페이지 번호 갯수를 3개로 지정
 		
 		// 3) 전체 페이지 목록 갯수 계산
-//		int maxpage = listCount/listLimit;
-//		// 페이지 목록 갯수 계산 후 나머지가 0보다 크면 페이지 갯수 +1 처리
-//		if((listCount%listLimit) >0) {
-//			maxpage++;
+//		int maxPage = listCount / listLimit;
+//		// 페이지 목록 갯수 계산 후 나머지가 0보다 크면 페이지 갯수 + 1 처리
+//		if(listCount % listLimit > 0) {
+//			maxPage++;
 //		}
-		int maxPage = listCount/listLimit + ((listCount%listLimit) > 0 ? 1 : 0 );
-		System.out.println("전체 페이지 목록 갯수 : " + maxPage);
+		// 위의 문장을 삼항연산자로 변환하여 동일한 작업 수행
+		// => 10개씩 나눈 나머지가 0보다 크면 + 1 처리 추가
+		int maxPage = listCount / listLimit + (listCount % listLimit > 0 ? 1 : 0);
+		
+//		System.out.println("전체 페이지 목록 갯수 : " + maxPage);
 		
 		// 4) 시작 페이지 번호 계산
 		// ex) pageListLimit = 3 일 때
-		int startPage = (pageNum-1)/pageListLimit * pageListLimit + 1;
+		//     1페이지 : (1 - 1) / 3 * 3 + 1 = 1, 2페이지 : (2 - 1) / 3 * 3 + 1 = 1, 3페이지 : (3 - 1) / 3 * 3 + 1 = 1 
+		//     4페이지 : (4 - 1) / 3 * 3 + 1 = 4, 5페이지 : (5 - 1) / 3 * 3 + 1 = 4, 6페이지 : (6 - 1) / 3 * 3 + 1 = 4 
+		int startPage = (pageNum - 1) / pageListLimit * pageListLimit + 1;
 //		System.out.println(startPage);
 		
 		// 5) 끝 페이지 번호 계산
-		// 시작 페이지 번호와 한 페이지 당 페이지 번호 갯수를 더한 값 -1
+		// 시작 페이지 번호와 한 페이지 당 페이지 번호 갯수를 더한 값 - 1
 		int endPage = startPage + pageListLimit - 1;
-		// --------------------------------------------------------------------
-		// 글목록(List 객체) 을 request 객체에 저장
-		request.setAttribute("boardList", boardList);
 		
-		//6) 만약, 끝 페이지 번호(endPage) 전체 페이지 번호 보다 클 경우 끝 페이지 번호를 최대 페이지 번호로 교체
-		if(endPage>maxPage) {
+		// 6) 만약, 끝 페이지 번호(endPage)가 전체(최대) 페이지 번호(maxPage) 보다 클 경우
+		//    끝 페이지 번호를 최대 페이지 번호로 교체
+		if(endPage > maxPage) {
 			endPage = maxPage;
 		}
+//		System.out.println(endPage);
 		
+		// 계산된 페이징 처리 관련 값을 PageInfo 객체에 저장
 		PageInfo pageInfo = new PageInfo(listCount, pageListLimit, maxPage, startPage, endPage);
+		// --------------------------------------------------------------------
+		// 글목록(List 객체)과 페이징정보(PageInfo 객체) 를 request 객체에 저장
 		request.setAttribute("boardList", boardList);
 		request.setAttribute("pageInfo", pageInfo);
 		
