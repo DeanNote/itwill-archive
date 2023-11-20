0b6bebc7d5df29e6d3b5ae6bae152b4961511f7e
Seokcoding <142768477+Git-Seokcoding@users.noreply.github.com>
2023-11-20 09:28:41 +0900

    11/20

--- MVC_Board/src/main/java/action/BoardModifyFormAction.java
+++ MVC_Board/src/main/java/action/BoardModifyFormAction.java
@@ -48,8 +48,11 @@ public class BoardModifyFormAction implements Action {
 		// => 파라미터 : 글번호   리턴타입 : BoardBean(board)
 		// => 주의! BoardModifyFormService 클래스를 새로 정의하는 것이 아니라
 		//    기존의 글 상세정보 조회 기능을 재사용하기 위해 BoardDetailService 클래스 재사용
+		
+		// => 글 수정, 답글 작성과 구분하여 조회수 증가도 함께 수행하기 위해
+		//    boolean타입 값을 신호로 전달(true : 조회수 증가, false : 조회수 미 증가)
 		BoardDetailService service = new BoardDetailService();
-		BoardBean board = service.getBoard(board_num);
+		BoardBean board = service.getBoard(board_num, false); // 조회수 미 증가
 //		System.out.println(board);
 		
 		// 세션 아이디와 조회 결과 중 작성자가 동일한지 판별
