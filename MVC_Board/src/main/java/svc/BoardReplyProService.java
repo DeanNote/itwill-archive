8056089d25e1d5d8ebc93798cc2ecb1e2fa14fea
Seokcoding <142768477+Git-Seokcoding@users.noreply.github.com>
2023-11-20 11:42:51 +0900

    11/20

---
+++ MVC_Board/src/main/java/svc/BoardReplyProService.java
@@ -0,0 +1,14 @@
+package svc;
+
+import vo.BoardBean;
+
+public class BoardReplyProService {
+
+	public boolean registReplyBoard(BoardBean board) {
+		boolean isWriteSuccess = false;
+		
+		
+		return isWriteSuccess;
+	}
+
+}
