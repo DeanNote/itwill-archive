ea140185fc4c4dce280c3744bb57758a659f7fac
Seokcoding <142768477+Git-Seokcoding@users.noreply.github.com>
2023-11-20 09:28:47 +0900

    11/20

--- MVC_Board/src/main/java/svc/BoardDetailService.java
+++ MVC_Board/src/main/java/svc/BoardDetailService.java
@@ -9,7 +9,7 @@ import vo.BoardBean;
 public class BoardDetailService {
 
 	// 게시물 상세정보 조회 요청 수행할 getBoard() 메서드
-	public BoardBean getBoard(int board_num) {
+	public BoardBean getBoard(int board_num, boolean isIncreaseReadcount) { // 조회수 증가 여부를 함께 전달받은 경우
 		BoardBean board = null;
 		
 		Connection con = JdbcUtil.getConnection();
@@ -24,15 +24,19 @@ public class BoardDetailService {
 		// 해당 게시물의 조회수(board_readcount) 증가 작업 요청
 		// BoardDAO - updateReadcount() 메서드 호출
 		// => 파라미터 : 글번호(board_num)   리턴타입 : int(updateCount)
-		int updateCount = dao.updateReadcount(board_num);
-		
-		// 작업 성공 시 commit 수행 및 BoardBean 객체의 readcount 값 1 증가시키고
-		// 아니면(실패 시) rollback 수행
-		if(updateCount > 0) { // 성공
-			JdbcUtil.commit(con);
-			board.setBoard_readcount(board.getBoard_readcount() + 1);
-		} else { // 실패
-			JdbcUtil.rollback(con);
+		// => 단, 게시물 조회 성공 조건과 함께 조회수 증가 신호(isIncreaseReadcount)값이 
+		//	  true 일 경우에만 조회수 증가 수행하도록 변경
+		if(board != null && isIncreaseReadcount) {
+			int updateCount = dao.updateReadcount(board_num);
+			
+			// 작업 성공 시 commit 수행 및 BoardBean 객체의 readcount 값 1 증가시키고
+			// 아니면(실패 시) rollback 수행
+			if(updateCount > 0) { // 성공
+				JdbcUtil.commit(con);
+				board.setBoard_readcount(board.getBoard_readcount() + 1);
+			} else { // 실패
+				JdbcUtil.rollback(con);
+			}
 		}
 		
 		JdbcUtil.close(con);
