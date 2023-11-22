2d516d3639ad23200046674722a2b3b938f2a4d9
Seokcoding <142768477+Git-Seokcoding@users.noreply.github.com>
2023-11-22 09:28:51 +0900

    11/22

--- MVC_Board/src/main/java/dao/BoardDAO.java
+++ MVC_Board/src/main/java/dao/BoardDAO.java
@@ -360,6 +360,84 @@ public class BoardDAO {
 		
 		return updateCount;
 	}
+
+	// 답글 쓰기
+	public int insertReplyBoard(BoardBean board) {
+		int insertCount = 0;
+		
+		PreparedStatement pstmt = null, pstmt2 = null;
+		ResultSet rs = null;
+		
+		try {
+			// 답글 쓰기에 필요한 답글 관련 정보들을 별도의 변수에 저장
+			int ref = board.getBoard_re_ref();		
+			int lev = board.getBoard_re_lev();		
+			int seq = board.getBoard_re_seq();		
+			
+			// 1. 기존 답글들에 대한 순서번호(board_re_seq) 증가 - UPDATE
+			String sql = "UPDATE board "
+						+ "SET board_re_seq = board_re_seq + 1 "
+						+ "WHERE "
+						+ "		board_re_ref = ? "
+						+ "		AND board_re_seq > ?";
+			pstmt = con.prepareStatement(sql);
+			pstmt.setInt(1, ref);
+			pstmt.setInt(2, seq);
+			pstmt.executeUpdate();
+			
+			// 기존 사용 완료된 pstmt 객체는 더 이상 불필요하므로 close()
+			pstmt.close();
+			// -----------------------------------------------------------
+			// 2. 답글 쓰기
+			// 2-1) 새 답글에 사용될 lev, seq 값은 원본값 + 1 처리
+			lev++;
+			seq++;
+			
+			// 2-2) 기존 게시물의 가장 큰 번호 조회 - SELECT => 글쓰기와 동일
+			int board_num = 1;
+			
+			sql = "SELECT MAX(board_num) FROM board";
+			pstmt = con.prepareStatement(sql);
+			rs = pstmt.executeQuery();
+			
+			if(rs.next()) { // 조회 결과가 있을 경우
+				board_num = rs.getInt(1) + 1; // 첫번째 컬럼값 + 1
+			}
+//			System.out.println("새 글 번호 : " + board_num);
+			// ------------------------------------------------------------------
+			// 전달받은 데이터(BoardBean 객체) 사용하여 글쓰기 작업 수행 - INSERT
+			// => 새 글 번호는 계산된 번호(board_num 값) 사용
+			// => 참조글번호(board_re_ref)는 새 글 번호와 동일한 번호 사용
+			// => 들여쓰기레벨(board_re_lev)과 순서번호(board_re_seq)는 0 지정
+			// => 조회수(board_readcount) 0 지정
+			// => 작성일자(board_date) 는 현재 서버 시각 활용(now() 함수 사용)
+			sql = "INSERT INTO board VALUES (?, ?, ?, ?, ?, ?, ?, ?, now(), ?)";
+			pstmt2 = con.prepareStatement(sql);
+			pstmt2.setInt(1, board_num); // 글번호(계산된 새 글 번호 사용)
+			pstmt2.setString(2, board.getBoard_name()); // 작성자(전달받은 값)
+			pstmt2.setString(3, board.getBoard_subject()); // 제목(전달받은 값)
+			pstmt2.setString(4, board.getBoard_content()); // 내용(전달받은 값)
+			pstmt2.setInt(5, ref); // 참조글번호(전달받은 값)
+			pstmt2.setInt(6, lev); // 들여쓰기레벨(계산된 값)
+			pstmt2.setInt(7, seq); // 순서번호(계산된 값)
+			pstmt2.setInt(8, 0); // 조회수(기본값 0)
+			pstmt2.setString(9, board.getWriter_ip()); // 작성자 IP 주소(전달받은 값)
+			
+			System.out.println(pstmt2); // 작성된 구문 확인
+			
+			insertCount = pstmt2.executeUpdate(); 
+		} catch (SQLException e) {
+			System.out.println("SQL 구문 오류 발생 - insertReplyBoard()");
+			e.printStackTrace();
+		} finally {
+			// DB 자원 반환
+			JdbcUtil.close(rs);
+			JdbcUtil.close(pstmt);
+			JdbcUtil.close(pstmt2);
+		}
+		
+		return insertCount;
+	}
 	
 	
 }
