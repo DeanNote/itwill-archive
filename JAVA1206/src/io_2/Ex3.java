a2b7cbd583bb541efe3c7546046779ec4d7f6f56
Seokcoding <142768477+Git-Seokcoding@users.noreply.github.com>
2023-12-06 15:36:27 +0900

    12/06

--- JAVA1206/src/io_2/Ex3.java
+++ JAVA1206/src/io_2/Ex3.java
@@ -58,17 +58,31 @@ public class Ex3 {
 			e.printStackTrace();
 		}
 		
-		System.out.println("--------------------------------------------------------");
+		System.out.println("====================================================");
 		
 		// 파일에 출력된 자바 기본데이터를 읽어와서 화면에 출력하기
-		try(DataInputStream dis = new DataInputStream(new FileInputStream("C://temp/data5.txt"))){
+		try(DataInputStream dis = new DataInputStream(new FileInputStream("C://temp/data5.txt"))) {
+			// dis.readXXX() 메서드를 호출하여 데이터 읽어오기(입력)
+			// 읽어들일 데이터는 반드시 출력된 데이터 순으로 읽어야 한다!
+			// => 출력 순서 : int -> double -> String 이므로, 입력 순서도 동일해야함
+			// => 순서가 바뀔 경우 EOFException 예외가 발생하므로 주의!
+			int num = dis.readInt();
+			double dNum = dis.readDouble();
+			String str = dis.readUTF();
 			
-		}catch(FileNotFoundException e) {
+			// 입력받은 데이터 출력
+			System.out.println("int형 정수 : " + num);
+			System.out.println("double형 정수 : " + dNum);
+			System.out.println("문자열 : " + str);
+			
+		} catch(FileNotFoundException e) {
+			// 읽어들일 파일이 존재하지 않거나 경로가 틀린 경우 예외 발생
 			e.printStackTrace();
-		}catch(IOException e) {
+		} catch(IOException e) {
 			e.printStackTrace();
 		}
-
+			
+		
 	}
 
 }
