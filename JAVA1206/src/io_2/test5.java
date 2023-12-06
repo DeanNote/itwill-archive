ef08a71e03fa26aee51d3b0515b821cf0959f2ac
Seokcoding <142768477+Git-Seokcoding@users.noreply.github.com>
2023-12-06 15:38:38 +0900

    12/06

--- JAVA1206/src/io_2/test5.java
+++ JAVA1206/src/io_2/test5.java
@@ -7,7 +7,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
 
-public class Test5 {
+public class test5 {
 
 	public static void main(String[] args) {
 		/*
