c0fbfe64307be208883f108612f465c0c1c33d9f
Seokcoding <142768477+Git-Seokcoding@users.noreply.github.com>
2023-12-08 09:03:30 +0900

    12/08

--- JAVA1206/src/io_2/Ex3.java
+++ JAVA1206/src/io_2/Ex3.java
@@ -11,30 +11,30 @@ public class Ex3 {
 
 	public static void main(String[] args) {
 		/*
-		 * 자바의 기본 데이터 입출력
-		 * - DataInputStream, DataOutputStream 사용
-		 *   => 메모리에 저장된 0, 1 상태를 그대로 읽거나 쓸 때 사용
-		 *   => 자료형의 크기가 그대로 보존됨
-		 * - 자바의 기본데이터입 8가지 + 문자열(String) 타입 처리 가능
-		 *   => readXXX(), writeXXX() 메서드를 사용하며 XXX 은 기본 데이터타입의 이름 사용
-		 *   ex) int형 데이터 출력시 : writeInt()
-		 *       double형 데이터 입력시 : readDouble()
-		 *   => 주의! String 타입은 XXX 부분에 String 대신 UTF 사용
-		 *   ex) readString() 메서드(X) -> readUTF() 메서드(O)
-		 *   
-		 * - 주의사항
-		 *   1) 반드시 두 객체 끼리만 데이터 입출력 가능
-		 *   2) 입출력 시 각각의 순서에 따라 처리해야함
-		 *   ex) int, char, String 순으로 출력시 int, char, String 순으로 입력해야 함
-		 *   
-		 * < 기본 문법 >
-		 * 1. 기본 데이터 출력
-		 *   DataOutputStream dos = new DataOutputStream(바이트 스트림 객체);
-		 *   dos.writeXXX(데이터);
-		 *   
-		 * 2. 기본 데이터 입력
-		 *   DataInputStream dis = new DataInputStream(바이트 스트림 객체);
-		 *   dis.readXXX(데이터);
+		  자바의 기본 데이터 입출력
+		  - DataInputStream, DataOutputStream 사용
+		    => 메모리에 저장된 0, 1 상태를 그대로 읽거나 쓸 때 사용
+		    => 자료형의 크기가 그대로 보존됨
+		  - 자바의 기본데이터입 8가지 + 문자열(String) 타입 처리 가능
+		    => readXXX(), writeXXX() 메서드를 사용하며 XXX 은 기본 데이터타입의 이름 사용
+		    ex) int형 데이터 출력시 : writeInt()
+		        double형 데이터 입력시 : readDouble()
+		    => 주의! String 타입은 XXX 부분에 String 대신 UTF 사용
+		    ex) readString() 메서드(X) -> readUTF() 메서드(O)
+		    
+		  - 주의사항
+		    1) 반드시 두 객체 끼리만 데이터 입출력 가능
+		    2) 입출력 시 각각의 순서에 따라 처리해야함
+		    ex) int, char, String 순으로 출력시 int, char, String 순으로 입력해야 함
+		    
+		  < 기본 문법 >
+		  1. 기본 데이터 출력
+		    DataOutputStream dos = new DataOutputStream(바이트 스트림 객체);
+		    dos.writeXXX(데이터);
+		    
+		  2. 기본 데이터 입력
+		    DataInputStream dis = new DataInputStream(바이트 스트림 객체);
+		    dis.readXXX(데이터);
 		 */
 		
 		// 자바 기본 데이터를 파일로 출력하기
--- JAVA1206/src/io_2/Ex5.java
+++ JAVA1206/src/io_2/Ex5.java
@@ -12,18 +12,18 @@ public class Ex5 {
 
 	public static void main(String[] args) {
 		/*
-		 * 키보드로부터 입력받은 데이터를 파일로 출력
-		 * 1. 키보드로부터 입력받기
-		 * 1) System.in을 통해 키보드로부터 입력받는 입력스트림 InputStream 객체로 연결
-		 *    => byte 단위로 처리
-		 * 2) InputStream -> InputStreamReader 객체로 변환하여 char 단위로 처리
-		 * 3) InputStreamReader -> BufferedReader 객체로 변환하여 String 단위로 처리
-		 * 4) BufferedReader 객체로부터 입력스트림 한줄(Line) 단위로 읽어와서 출력
-		 * 
-		 * 2. 파일로 출력하기
-		 * 1) File 객체를 사용하여 출력할 파일 위치 및 이름을 지정
-		 * 2) FileWriter 객체를 사용하여 char 단위로 처리(File 객체 전달)
-		 * 3) FileWriter -> PrintWriter 객체로 변환하여 출력
+		  키보드로부터 입력받은 데이터를 파일로 출력
+		  1. 키보드로부터 입력받기
+		  1) System.in을 통해 키보드로부터 입력받는 입력스트림 InputStream 객체로 연결
+		     => byte 단위로 처리
+		  2) InputStream -> InputStreamReader 객체로 변환하여 char 단위로 처리
+		  3) InputStreamReader -> BufferedReader 객체로 변환하여 String 단위로 처리
+		  4) BufferedReader 객체로부터 입력스트림 한줄(Line) 단위로 읽어와서 출력
+		  
+		  2. 파일로 출력하기
+		  1) File 객체를 사용하여 출력할 파일 위치 및 이름을 지정
+		  2) FileWriter 객체를 사용하여 char 단위로 처리(File 객체 전달)
+		  3) FileWriter -> PrintWriter 객체로 변환하여 출력
 		 */
 
 //		InputStream is = System.in;
