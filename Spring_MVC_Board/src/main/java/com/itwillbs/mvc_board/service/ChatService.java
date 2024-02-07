88f70eb3a9101bed942c58aef698836d8e819923
Seokcoding <142768477+Git-Seokcoding@users.noreply.github.com>
2024-02-07 11:18:57 +0900

    2/7

---
+++ Spring_MVC_Board/src/main/java/com/itwillbs/mvc_board/service/ChatService.java
@@ -0,0 +1,46 @@
+package com.itwillbs.mvc_board.service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.itwillbs.mvc_board.mapper.ChatMapper;
+import com.itwillbs.mvc_board.vo.ChatMessage2;
+import com.itwillbs.mvc_board.vo.ChatRoomVO;
+
+@Service
+public class ChatService {
+	@Autowired
+	private ChatMapper mapper;
+
+	// 새 채팅방 정보 저장 요청
+	public void addChatRoom(ChatMessage2 chatMessage) {
+		// 1개 채팅방 정보(룸ID, 사용자ID)를 갖는 ChatRoomVO 객체 생성
+		// => 이 때, 2명의 사용자 정보를 각각의 레코드로 추가하기 위해 List 객체로 저장
+		// => 채팅방 제목은 "채팅-상대방아이디" 형식으로 지정
+		List<ChatRoomVO> chatRoom = new ArrayList<ChatRoomVO>();
+		chatRoom.add(new ChatRoomVO(chatMessage.getRoom_id(), chatMessage.getSender_id(), "채팅-" + chatMessage.getReceiver_id(), null));
+		chatRoom.add(new ChatRoomVO(chatMessage.getRoom_id(), chatMessage.getReceiver_id(), "채팅-" + chatMessage.getSender_id(), null));
+		mapper.insertChatRoom(chatRoom);
+	}
+
+	// 채팅방 목록 조회 요청
+	public List<ChatRoomVO> getChatRoomList(String sender_id) {
+		return mapper.selectChatRoomList(sender_id);
+	}
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
